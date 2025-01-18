5b99cf6a504d85b0f4d1423a5b1e922b35452bdb
Antonio Valent <avalent@foi.hr>
2025-01-18 15:00:11 +0100

    Added useState hook and implemented delete function

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+  const {children, handleDeleteTodo, index} = props
+  return (
+    <li className='todoItem'> 
+      {children}
+      <div className='actionsContainer'>
+        <button>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button onClick={() => {
+          handleDeleteTodo(index)
+        }}>
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    </li>
+  )
+}
